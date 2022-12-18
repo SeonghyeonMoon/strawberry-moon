1e8b4c0c989623f75850084bd00bf2011f31f333
문성현 <shine7329@gmail.com>
2022-12-18 21:49:45 +0900

    ✨Feature: 달력 스타일 추가

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import makeCalendar from '../utils/makeCalendar';
 import type { Calendar } from '../type';
-import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
@@ -20,33 +20,57 @@ const Calendar = () => {
   }, [date]);
 
   return (
-    <div>
-      <h1 className='mb-5 flex items-center gap-2 text-2xl'>
+    <div className='mx-auto mt-7 flex w-1/2 flex-col'>
+      <h1 className='mb-5 flex items-center justify-center gap-7 text-2xl'>
         <button onClick={moveToPrevMonth}>
-          <AiFillCaretLeft />
+          <AiOutlineLeft />
         </button>
         {date.getFullYear()}년 {date.getMonth() + 1}월
         <button onClick={moveToNextMonth}>
-          <AiFillCaretRight />
+          <AiOutlineRight />
         </button>
       </h1>
       <table>
         <thead>
           <tr>
-            <td>일</td>
-            <td>월</td>
-            <td>화</td>
-            <td>수</td>
-            <td>목</td>
-            <td>금</td>
-            <td>토</td>
+            {['일', '월', '화', '수', '목', '금', '토'].map((day) => (
+              <td
+                key={day}
+                className={`py-5 text-center font-bold 
+                ${day === '토' ? 'text-blue-600' : ''} 
+                ${day === '일' ? 'text-red-600' : ''}`}
+              >
+                {day}
+              </td>
+            ))}
           </tr>
         </thead>
         <tbody>
           {calendarData?.map((week, index) => (
             <tr key={index}>
-              {week?.map((date, index) => (
-                <td key={index}>{date ? <span>{date}</span> : ''}</td>
+              {week?.map(({ date, isToday }, index) => (
+                <td
+                  key={index}
+                  className={`py-5 text-center 
+                  ${index === 0 ? 'text-red-600' : ''} 
+                  ${index === 6 ? 'text-blue-600' : ''}`}
+                >
+                  {date ? (
+                    isToday ? (
+                      <span
+                        className={`border-b-2 border-black font-bold
+                        ${index === 0 ? 'border-red-600' : ''} 
+                        ${index === 6 ? 'border-blue-600' : ''}`}
+                      >
+                        {date}
+                      </span>
+                    ) : (
+                      <span>{date}</span>
+                    )
+                  ) : (
+                    ''
+                  )}
+                </td>
               ))}
             </tr>
           ))}
