6072c6cb33fb2ad3b39f47c64aa10426ddf8c8e5
문성현 <shine7329@gmail.com>
2022-12-21 22:42:58 +0900

    🔨Refactor: response 앞으로 return문 위치 변경

--- pages/api/count.ts
+++ pages/api/count.ts
@@ -16,43 +16,39 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (month) {
       const monthCheckReg = /^[0-9]{6}$/;
       if (!monthCheckReg.test(month as string)) {
-        res
+        return res
           .status(400)
           .json({ error: '올바르지 않은 월 형식의 파라미터입니다.' });
-        return;
       }
-      res
+      return res
         .status(200)
         .json(
           count
             .filter(({ date }) => date.startsWith(month as string))
             .sort((a, b) => (Number(a.date) - Number(b.date) > 0 ? 1 : -1)),
         );
-      return;
     }
     if (date) {
       const dateCheckReg = /^[0-9]{8}$/;
       if (!dateCheckReg.test(date as string)) {
-        res
+        return res
           .status(400)
           .json({ error: '올바르지 않은 일 형식의 파라미터입니다.' });
-        return;
       }
       const result = count.find(({ date: dateData }) => dateData === date);
       if (result) {
-        res.status(200).json(result);
+        return res.status(200).json(result);
       }
-      res.status(404).json({ error: '해당 날짜의 데이터가 없습니다.' });
+      return res.status(404).json({ error: '해당 날짜의 데이터가 없습니다.' });
     }
   }
 
   if (req.method === 'POST') {
     const dateCheckReg = /^[0-9]{8}$/;
     if (!dateCheckReg.test(req.body.date as string)) {
-      res
+      return res
         .status(400)
         .json({ error: '올바르지 않은 일 형식의 쿼리요청입니다.' });
-      return;
     }
     const index = count.findIndex(({ date }) => date === req.body.date);
     if (index === -1) {
@@ -60,6 +56,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     } else {
       count.splice(index, 1, req.body);
     }
-    res.status(201).json(req.body);
+    return res.status(201).json(req.body);
   }
 }
