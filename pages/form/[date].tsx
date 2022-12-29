71f426772526eff374cf8916ffa31651ab41b35e
문성현 <shine7329@gmail.com>
2022-12-29 16:14:41 +0900

    ✨Feature: 캘린더 메인 페이지 설정, 페이지 제목 설정

--- pages/form/[date].tsx
+++ pages/form/[date].tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useEffect, useState } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ToastContainer } from 'react-toastify';
@@ -87,62 +88,72 @@ const Date = () => {
   }
 
   return (
-    <form
-      className='relative flex w-full flex-col gap-2 py-8 px-4'
-      onSubmit={handleSubmit}
-    >
-      <ToastContainer
-        position='top-center'
-        autoClose={500}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-        theme='light'
-      />
-      <FormHeader date={router.query.date} />
-      <table className='mb-4'>
-        <FormTableHeader />
-        <tbody>
-          {Object.entries(formData).map(([grade, { label, price, count }]) => (
-            <tr key={grade} className='border text-left'>
-              <td className='w-10 border p-2'>{label}</td>
-              <td className='relative w-16 border'>
-                <NumberInput
-                  type='count'
-                  value={count}
-                  grade={grade as Grade}
-                  handleChange={handleChange}
-                />
-              </td>
-              <td className='relative w-16 border'>
-                <NumberInput
-                  type='price'
-                  value={price}
-                  grade={grade as Grade}
-                  handleChange={handleChange}
-                />
-              </td>
-              <td className='w-24 border p-2'>
-                <span>{(count * price).toLocaleString()}</span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button
-        type='submit'
-        disabled={!isFormChanged}
-        className='rounded bg-gray-600 p-2 text-white transition disabled:opacity-10'
+    <>
+      <Head>
+        <title>{`${router.query.date?.slice(0, 4)}년 ${router.query.date?.slice(
+          4,
+          6,
+        )}월 ${router.query.date?.slice(6)}일`}</title>
+      </Head>
+      <form
+        className='relative flex w-full flex-col gap-2 py-8 px-4'
+        onSubmit={handleSubmit}
       >
-        등록
-      </button>
-      <Link
-        href={'/calendar'}
-        className='block rounded p-2 text-center text-gray-400'
-      >
-        취소
-      </Link>
-    </form>
+        <ToastContainer
+          position='top-center'
+          autoClose={500}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          theme='light'
+        />
+        <FormHeader date={router.query.date} />
+        <table className='mb-4'>
+          <FormTableHeader />
+          <tbody>
+            {Object.entries(formData).map(
+              ([grade, { label, price, count }]) => (
+                <tr key={grade} className='border text-left'>
+                  <td className='w-10 border p-2'>{label}</td>
+                  <td className='relative w-16 border'>
+                    <NumberInput
+                      type='count'
+                      value={count}
+                      grade={grade as Grade}
+                      handleChange={handleChange}
+                    />
+                  </td>
+                  <td className='relative w-16 border'>
+                    <NumberInput
+                      type='price'
+                      value={price}
+                      grade={grade as Grade}
+                      handleChange={handleChange}
+                    />
+                  </td>
+                  <td className='w-24 border p-2'>
+                    <span>{(count * price).toLocaleString()}</span>
+                  </td>
+                </tr>
+              ),
+            )}
+          </tbody>
+        </table>
+        <button
+          type='submit'
+          disabled={!isFormChanged}
+          className='rounded bg-gray-600 p-2 text-white transition disabled:opacity-10'
+        >
+          등록
+        </button>
+        <Link
+          href={'/'}
+          className='block rounded p-2 text-center text-gray-400'
+        >
+          취소
+        </Link>
+      </form>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,89 @@
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { CategoryScale } from 'chart.js';
+import Chart from 'chart.js/auto';
+import { CalendarHeader, Days, Td } from '../components/calendar';
+import Loading from '../components/Loading';
+import MonthCountChart from '../components/MonthCountChart';
+import useSwipe from '../hooks/useSwipe';
+import makeCalendar from '../utils/makeCalendar';
+import type { Calendar } from '../type';
 
-export default function Home() {
+Chart.register(CategoryScale);
+
+const Home = () => {
+  const [dateData, setDateData] = useState(new Date());
+  const [calendarData, setCalendarData] = useState<Calendar>();
+  const [initLoading, setInitLoading] = useState(true);
+
+  const moveToPrevMonth = () => {
+    setDateData(new Date(dateData.getFullYear(), dateData.getMonth() - 1));
+  };
+
+  const moveToNextMonth = () => {
+    setDateData(new Date(dateData.getFullYear(), dateData.getMonth() + 1));
+  };
+
+  useSwipe({
+    handleSwipeRight: moveToPrevMonth,
+    handleSwipeLeft: moveToNextMonth,
+  });
+
+  useEffect(() => {
+    setCalendarData(makeCalendar(dateData.getFullYear(), dateData.getMonth()));
+  }, [dateData]);
+
+  useEffect(() => {
+    setInitLoading(true);
+    const timeOut = setTimeout(() => {
+      setInitLoading(false);
+    }, 500);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [dateData]);
+
+  if (initLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <Link href={'/calendar'}>Calendar</Link>
+      <Head>
+        <title>{`${dateData.getFullYear()}년 ${
+          dateData.getMonth() + 1
+        }월`}</title>
+      </Head>
+      <div className='mx-auto mt-5 flex w-full flex-col p-5'>
+        <CalendarHeader
+          date={dateData}
+          moveToPrevMonth={moveToPrevMonth}
+          moveToNextMonth={moveToNextMonth}
+        />
+        <table className='mb-8'>
+          <Days />
+          <tbody>
+            {calendarData?.map((week, index) => (
+              <tr key={index}>
+                {week?.map(({ date, isToday }, index) => (
+                  <Td
+                    key={index}
+                    index={index}
+                    dateData={dateData}
+                    date={date}
+                    isToday={isToday}
+                  />
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <MonthCountChart
+          date={`${dateData.getFullYear()}${dateData.getMonth() + 1}`}
+        />
+      </div>
     </>
   );
-}
+};
+
+export default Home;
